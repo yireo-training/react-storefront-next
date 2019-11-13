f127e755f002245b8b28dde232724817f9f3a8b8
adrien-k <adrien-k@users.noreply.github.com>
2019-11-13 18:43:17 +0800

    fix(api/routes): lambda compatibility (#15)

--- pages/api/routes.js
+++ pages/api/routes.js
@@ -2,14 +2,27 @@ import getRoutes from 'react-storefront/server/getRoutes'
 import fs from 'fs'
 import path from 'path'
 
-export default function routes(req, res) {
-  try {
-    const manifest = JSON.parse(
-      fs.readFileSync(path.join(process.cwd(), '.next', 'server', 'pages-manifest.json'), 'utf-8')
-    )
-    res.end(JSON.stringify(getRoutes(manifest)))
-  } catch (e) {
-    console.error(e)
-    res.end('{}')
+// Depending on the context (local dev, serverless lambda), the page manifest
+// file can be at different locations
+const MANIFEST_PATHS = [
+  path.join(process.cwd(), 'pages-manifest.json'),
+  path.join(process.cwd(), '.next', 'server', 'pages-manifest.json'),
+]
+
+let routesJson = '{}'
+try {
+  const manifestPath = MANIFEST_PATHS.find((path) => fs.existsSync(path))
+  if (manifestPath) {
+    const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf-8'))
+    const routes = getRoutes(manifest)
+    routesJson = JSON.stringify(routes)
+  } else {
+    throw new Error(`Could not find pages-manifests.json in ${MANIFEST_PATHS.join(' or ')}`)
   }
+} catch (e) {
+  console.error(e)
+}
+
+export default function routes(req, res) {
+  res.end(routesJson)
 }
